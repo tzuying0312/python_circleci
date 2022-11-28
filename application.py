7f3f328c8f169826f50e7e3387bd9380a57a63d4
tzuying0312 <tzuying0312@gmail.com>
2022-11-28 14:41:52 +0800

    test return

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
 def home():
-    return "Hello World!"
+    return "Hello World! 11/28"
 
 def init_api():
     application.run(debug=False, use_reloader=False)
